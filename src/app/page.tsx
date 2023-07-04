d5f22c600c7845a593fe89d3d075621320a697ad
eug-vs <eugene@eug-vs.xyz>
2023-07-04 21:34:02 +0300

    feat: revalidate page every hour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import ProjectsList from './components/ProjectsList';
 import MembersList from './components/MembersList';
-import ActivitiesList from "./components/ActivitiesList";
+import ActivitiesList from './components/ActivitiesList';
 import HubstaffClient from './hubstaffClient';
+
+export const revalidate = 3600;
 
 export default async function Home() {
   const client = new HubstaffClient();
@@ -10,9 +12,7 @@ export default async function Home() {
   );
   const members = await client.getOrganizationUsers();
   const activities = await client.getActivities(
-    process.env.ORGANIZATION_ID
-    ? Number(process.env.ORGANIZATION_ID)
-    : 0,
+    process.env.ORGANIZATION_ID ? Number(process.env.ORGANIZATION_ID) : 0,
     {
       startTime: new Date(2023, 1, 1),
       stopTime: new Date(2023, 1, 5),
