6b6f9a334460b14e4b6be3077cee7e17ed197406
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-07-14 15:42:04 +0300

    feat: add date interval inputs

---
+++ src/app/components/HoursTracked.tsx
@@ -0,0 +1,72 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { type HubstaffActivity } from '../hubstaffValidators';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface Props {
+  activities: HubstaffActivity[];
+}
+
+const HoursTracked = ({ activities }: Props) => {
+  const params = useSearchParams();
+  const [startDate, setStartDate] = useState(
+    params.get("startDate")
+    ? new Date(String(params.get("startDate")))
+    : new Date("2023-01-01")
+  );
+  const [endDate, setEndDate] = useState(
+    params.get("endDate")
+    ? new Date(String(params.get("endDate")))
+    : new Date("2023-01-08")
+  );
+  const router = useRouter();
+
+  const handleChangeStartDate = (date: Date) => {
+    setStartDate(date);
+    router.push("/?startDate=" + date.toISOString().slice(0,10) + "&endDate=" + endDate.toISOString().slice(0,10));
+  }
+
+  const handleChangEndDate = (date: Date) => {
+    setEndDate(date);
+    router.push("/?startDate=" + startDate.toISOString().slice(0,10) + "&endDate=" + date.toISOString().slice(0,10));
+  }
+  
+  const trackedTime = activities.reduce(
+    (sum, activity) => (activity.tracked ? sum + activity.tracked : sum),
+    0
+  );
+
+  return (
+    <div>
+      <div className="text-primary mb-2 mt-0 text-5xl font-medium leading-tight">
+        Hours tracked
+      </div>
+      <div className="text-primary mb-2 mt-0 text-2xl leading-tight">
+        Start date
+      </div>
+      <input
+        className="mb-8 block h-16 w-80 rounded-xl border-2 border-solid border-black p-2"
+        type="date"
+        placeholder="Start date"
+        onChange={(e) => handleChangeStartDate(new Date(e.target.value))}
+        value={String(params.get("startDate"))}
+      />
+      <div className="text-primary mb-2 mt-0 text-2xl leading-tight">
+        End date
+      </div>
+      <input
+        className="mb-8 block h-16 w-80 rounded-xl border-2 border-solid border-black p-2"
+        type="date"
+        placeholder="End date"
+        onChange={(e) => handleChangEndDate(new Date(e.target.value))}
+        value={String(params.get("endDate"))}
+      />
+      <div className="text-primary mb-2 mt-0 text-2xl leading-tight">
+        Hours: {Math.trunc(trackedTime / 3600)}
+      </div>
+    </div>
+  );
+};
+
+export default HoursTracked;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 import ActivitiesList from './components/ActivitiesList';
-import ActivitiesSum from './components/ActivitiesSum';
+import HoursTracked from './components/HoursTracked';
 import MembersList from './components/MembersList';
 import ProjectsList from './components/ProjectsList';
 import HubstaffClient from './hubstaffClient';
 
-export default async function Home() {
+export default async function Home({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
   const client = new HubstaffClient();
   const members = await client.getOrganizationMembers();
   const projects = await client.getProjects();
-  const activities = await client.getActivities(
+  /*const activities = await client.getActivities(
     new Date('2023-07-01'),
     new Date('2023-07-08')
+  );*/
+  const activities = await client.getActivities(
+    new Date(searchParams.startDate ? String(searchParams.startDate) : '2023-07-01'),
+    new Date(searchParams.startDate ? String(searchParams.endDate) : '2023-07-08')
   );
 
   return (
     <main className="container mx-auto py-10">
-      <ActivitiesSum activities={activities} />
+      <HoursTracked activities={activities} />
       <MembersList members={members} />
       <ProjectsList projects={projects} />
       <ActivitiesList activities={activities} />
